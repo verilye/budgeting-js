78bf327f7600dfa80167262c3c2e850fc9d03ab7
verilye <connor.j.logan@outlook.com>
2023-01-19 02:07:51 +1000

    login displays and is hidden on click

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import LoginComponent from './splash/login-component';
 import Splash from './splash/splash';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Splash/>
-    </div>
+    </>
   );
 }
 
--- front-end/src/splash/splash.js
+++ front-end/src/splash/splash.js
@@ -1,36 +1,36 @@
 import React from "react";
+import { useState } from "react";
 import LoginComponent from "./login-component";
 import "./splash.css";
 
+export default function Splash(){
 
-export default class Splash extends React.Component{
+    const [loginVisible, setLoginVisible] = useState(false);
 
-    render(){
+    const toggleLogin = event =>{
+        setLoginVisible(!loginVisible);
+    };
 
-        return(
-            <div className='splash'>
-               
-                <div className='graphic'>
-                    <div className='graphic-01 graphic-item'></div>
-                    <div className='graphic-02 graphic-item'> </div>
-                    <div className='graphic-03 graphic-item'></div>
-                    <div className='graphic-04 graphic-item'></div>
-                </div>
-                
-                <div className='menu-items'>
-                    <div className='title'>budgeting-js</div>
-                    <div className='login item'>
-                        login 
-                        <div className='item'>
-                            <LoginComponent/>
-                        </div>
-                    </div> 
-                    <div className='features item'>features</div>
-                    <div className='about item'>about</div>
-                </div>
+
+    return(
+        <div className='splash'>
+            <div className='graphic'>
+                <div className='graphic-01 graphic-item'></div>
+                <div className='graphic-02 graphic-item'> </div>
+                <div className='graphic-03 graphic-item'></div>
+                <div className='graphic-04 graphic-item'></div>
             </div>
-        )
-    }
-
-
+            
+            <div className='menu-items'>
+                <div className='title'>budgeting-js</div>
+                <div className='login item' onClick={toggleLogin}>
+                    login
+                        {loginVisible && <LoginComponent/>}
+                </div> 
+                <div className='features item'>features</div>
+                <div className='about item'>about</div>
+            </div>
+        </div>
+    )
+    
 }
